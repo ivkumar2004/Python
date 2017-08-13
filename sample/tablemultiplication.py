422aa468f09ef69efa25e33f3b3a4f900d5acd84
ivkumar2004 <ivkumar2004@gmail.com>
2017-08-13 19:56:23 +0530

    tablemultiplication
    Script to generate multiplication table

---
+++ sample/tablemultiplication.py
@@ -0,0 +1,18 @@
+
+def mathstable():
+	"""Function to generate table from sequence 1 to 12, Number get from user"""
+	tableInt = int(raw_input("Enter the number to generate its multiples: "))
+	for sequence in range(1,13):
+		print "%s x %s = %s"%(tableInt,sequence,tableInt*sequence)
+	return
+
+if __name__ == "__main__":
+	exitOperation = False
+	while exitOperation != True:
+		ynString = str(raw_input("Do you want to continue generating table (y/n): ")).strip().lower()
+		if ynString == "y":
+			mathstable()
+		elif ynString == "n":
+			exitOperation = True
+		else:
+			print "Illegal string %s"%ynString
