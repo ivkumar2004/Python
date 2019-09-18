8580b1135e8f81f70d7b8c4288dc2e6084adbc4a
Vinoth Kumar I <ivkumar2004@gmail.com>
2019-09-18 21:36:01 +0530

    Update tablemultiplication.py
    python 2 to 3 update

--- sample/tablemultiplication.py
+++ sample/tablemultiplication.py
@@ -1,18 +1,18 @@
 
 def mathstable():
 	"""Function to generate table from sequence 1 to 12, Number get from user"""
-	tableInt = int(raw_input("Enter the number to generate its multiples: "))
+	tableInt = int(input("Enter the number to generate its multiples: "))
 	for sequence in range(1,13):
-		print "%s x %s = %s"%(tableInt,sequence,tableInt*sequence)
+		print ("%s x %s = %s"%(tableInt,sequence,tableInt*sequence))
 	return
 
 if __name__ == "__main__":
 	exitOperation = False
 	while exitOperation != True:
-		ynString = str(raw_input("Do you want to continue generating table (y/n): ")).strip().lower()
+		ynString = str(input("Do you want to continue generating table (y/n): ")).strip().lower()
 		if ynString == "y":
 			mathstable()
 		elif ynString == "n":
 			exitOperation = True
 		else:
-			print "Illegal string %s"%ynString
+			print ("Illegal string %s"%ynString)
