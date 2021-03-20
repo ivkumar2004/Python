5212f4a61719b43b9a8fc0ea17aafb5d9d2d38d7
Vinoth Kumar I <ivkumar2004@gmail.com>
2021-03-20 11:42:33 +0530

    added external driver

--- selenium/1_selenium.py
+++ selenium/1_selenium.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager #only used to install the webdriver in runtime
 
 # WebDriverManager to install and invoke the driver for Chrome/Firefox/IE
-driver = webdriver.Chrome(ChromeDriverManager().install())
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(executable_path = r'C:\Users\ivkum\AppData\Local\Programs\Python\Python38-32\chromedriver.exe')
 driver.get("https://www.facebook.com")
 driver.close()
